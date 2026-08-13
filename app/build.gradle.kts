4ea5c0af6663b75f2efd91fd037cd50557f7cbf9
sameerasw <sameerasw.com@gmail.com>
2026-08-13 19:25:23 +0530

    chore: bump version code to 6 and version name to 6.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.sameerasw.essentials"
         minSdk = 34
         targetSdk = 36
-        versionCode = 5
-        versionName = "5.0"
+        versionCode = 6
+        versionName = "6.0"
 
     }
 
